a40647e9937230a4503b6cceb2e6143ab239b611
NazarStf <hworker98@gmail.com>
2023-07-27 21:43:11 +0300

    Initial Commit

---
+++ Animating Circles/Animating_CirclesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Animating_CirclesApp.swift
+//  Animating Circles
+//
+//  Created by NazarStf on 27.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct Animating_CirclesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
